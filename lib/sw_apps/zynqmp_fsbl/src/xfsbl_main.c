9afcc1af8bbe481d02523b86138e46a3a0568bc0
Joshua Hernstrom <joshua.hernstrom@ni.com>
2025-04-11 11:40:16 -0500

    Add debug print

--- lib/sw_apps/zynqmp_fsbl/src/xfsbl_main.c
+++ lib/sw_apps/zynqmp_fsbl/src/xfsbl_main.c
@@ -365,7 +365,7 @@ void XFsbl_PrintFsblBanner(void )
                  "Release %d.%d   %s  -  %s\r\n",
                  SDK_RELEASE_YEAR, SDK_RELEASE_QUARTER,__DATE__,__TIME__);
 
-	XFsbl_Printf(DEBUG_GENERAL, "MultiBootOffset: 0x%0x\r\n",
+	XFsbl_Printf(DEBUG_PRINT_ALWAYS, "MultiBootOffset: 0x%0x\r\n",
 		XFsbl_In32(CSU_CSU_MULTI_BOOT));
 
 	if(FsblInstance.ResetReason == XFSBL_PS_ONLY_RESET) {
